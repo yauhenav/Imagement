4d62ca8e11de1463320280e8f0bc8eb9ae757e37
yauhenav <enavitski@gmail.com>
2017-10-30 16:29:04 +0300

    upload file function working OK

--- src/main/java/com/yauhenav/logic/service/SessionUtilProd.java
+++ src/main/java/com/yauhenav/logic/service/SessionUtilProd.java
@@ -1,35 +1,26 @@
 package com.yauhenav.logic.service;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+
 public class SessionUtilProd {
-    private static final SessionFactory sessionFactory;
 
-    static {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
         try {
-			//sessionFactory = new Configuration().configure().buildSessionFactory();
-
-            Configuration configuration = new Configuration();
-            configuration = configuration.configure();
-            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
-            builder = builder.applySettings(configuration.getProperties());
-            sessionFactory = configuration.buildSessionFactory(builder.build());
-
-            //System.out.println(sessionFactory.toString());
-        } catch (Throwable ex) {
-            System.err.println("SessionFactory creation failed." + ex);
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure().buildSessionFactory();
+        }
+        catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
-    }
-
-    public static void shutdown() {
-        // Close caches and connection pools
-        getSessionFactory().close();
     }
 }
